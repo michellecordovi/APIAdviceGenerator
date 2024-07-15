d70ba8170f5103bbb7ec6f5701a5bf464c3f79cf
MichelleCordovi <michellecordovi@gmail.com>
2024-07-15 06:34:42 -0400

    add advice API

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ diceButton.onclick = () => {
        diceButton.style.transform = "rotate(1turn)";
 }
 
+const getAdvice = async () => {
+      const endpoint = 'https://api.adviceslip.com/advice'
+      try {
+            const response = await fetch(endpoint);
+            if (response.ok) {
+                  const JSresponse = await response.json()
+                  console.log(JSresponse.slip.advice)
+                  console.log(JSresponse.slip.id)
+            }
+      } catch (error) {
+             console.log(error)
+       }
+}
+
+getAdvice();
