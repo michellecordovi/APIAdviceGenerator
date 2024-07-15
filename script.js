161073616a9fa82a012d3b8c556550d6a89d0def
MichelleCordovi <michellecordovi@gmail.com>
2024-07-15 08:25:49 -0400

    add click event to update random advice

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 const diceButton = document.getElementById("dice-btn");
 
 diceButton.onclick = () => {
-       diceButton.style.transform = "rotate(1turn)";
+      diceButton.style.boxShadow = "0px 0px 20px hsl(150, 100%, 66%)"
+      diceButton.style.transition = "2s";
+      diceButton.style.transform = "rotate(1turn)";
+      getAdvice();
 }
+
+
+//fetch random advice
+const adviceNum = document.getElementById("advice-number");
+const randomAdvice = document.getElementById("quote")
 
 const getAdvice = async () => {
       const endpoint = 'https://api.adviceslip.com/advice'
@@ -11,8 +19,11 @@ const getAdvice = async () => {
             const response = await fetch(endpoint);
             if (response.ok) {
                   const JSresponse = await response.json()
-                  console.log(JSresponse.slip.advice)
-                  console.log(JSresponse.slip.id)
+                  randomAdvice.innerHTML = JSresponse.slip.advice;
+                  adviceNum.innerHTML = JSresponse.slip.id;
+                  diceButton.style.boxShadow = "none"
+                  diceButton.style.transform = "initial";
+                  diceButton.style.transition = "none";
             }
       } catch (error) {
              console.log(error)
@@ -20,3 +31,4 @@ const getAdvice = async () => {
 }
 
 getAdvice();
+
