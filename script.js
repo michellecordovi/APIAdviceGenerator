26cd1333125f70ca5e26772acc680edb6f807d21
MichelleCordovi <michellecordovi@gmail.com>
2024-07-15 15:11:46 -0400

    adjust transitions for dice button on click

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const diceButton = document.getElementById("dice-btn");
 
 diceButton.onclick = () => {
+      diceButton.style.transition = "box-shadow 1s, transform 1.5s linear";
       diceButton.style.boxShadow = "0px 0px 20px hsl(150, 100%, 66%)"
-      diceButton.style.transition = "2s";
       diceButton.style.transform = "rotate(1turn)";
       getAdvice();
 }
@@ -13,21 +13,25 @@ diceButton.onclick = () => {
 const adviceNum = document.getElementById("advice-number");
 const randomAdvice = document.getElementById("quote")
 
+
 const getAdvice = async () => {
-      const endpoint = 'https://api.adviceslip.com/advice'
+      const endpoint = 'https://api.adviceslip.com/advice';
       try {
             const response = await fetch(endpoint);
             if (response.ok) {
                   const JSresponse = await response.json()
                   randomAdvice.innerHTML = JSresponse.slip.advice;
                   adviceNum.innerHTML = JSresponse.slip.id;
-                  diceButton.style.boxShadow = "none"
-                  diceButton.style.transform = "initial";
-                  diceButton.style.transition = "none";
+                  
+                  setTimeout(() => {
+                        diceButton.style.transition = "box-shadow 0.5s, transform 0s";
+                        diceButton.style.boxShadow = "none";
+                        diceButton.style.transform = "none";
+                  }, 1000)
             }
       } catch (error) {
              console.log(error)
-       }
+      }
 }
 
 getAdvice();
